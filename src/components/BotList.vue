020529f05a985c04e883b29ccb723d117666487c
Bonnie Jordan <thebonstergirl@gmail.com>
2018-10-05 18:26:26 -0500

    battle bots updated

---
+++ src/components/BotList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <h1 :key="ind" v-for="(bot, ind) in botArr"> {{botArr.name}}</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        botArr: botArr
+        }
+    }
+</script>
+
---
+++ src/views/Create.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+    <form @submit.prevent="createBot(name, attack, health)">
+    <h2>Name:</h2><input type="text" v-model="name"/><br/>
+    <h2>Attack:</h2><input type="text" v-model="attack"/><br/>
+    <h2>Health:</h2><input type="text" v-model="health"/><br/>
+    <router-link to="/BotBattle"><button @click="createBot">Create my bot! </button></router-link>
+    </form>
+    <BotList :botArr="botArr"/>
+    </div>
+</template>
+<script>
+import BotList from '@/component/BotList.vue'
+export default {
+    data(){
+        return {
+            botArr: [
+                {name: 'Metroid',
+            attack: '100',
+            health: '200'},
+            {name: 'Allen',
+            attack: '40',
+            health: '201'},
+            {name: 'Hilda',
+            attack: '70',
+            health: '300'}
+            ],
+            name: '',
+            attack: '',
+            health: ''
+        }
+    },
+    methods: {
+        createBot(){
+            console.log(this.botArr)
+            this.botArr.push({name: this.name,
+            attack: this.attack,
+            health: this.health});
+            this.name = '',
+            this.attack = '',
+            this.health = ''
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
+
